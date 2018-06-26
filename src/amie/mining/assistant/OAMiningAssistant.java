6459f5f60546055d0e10a0f2952f3fc4d774589b
hesslem <Klondike7893>
2018-06-26 21:24:29 +0200

    Create Amie_OA repo

---
+++ src/amie/mining/assistant/OAMiningAssistant.java
@@ -0,0 +1,29 @@
+package amie.mining.assistant;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javatools.datatypes.ByteString;
+import javatools.datatypes.IntHashMap;
+import javatools.datatypes.Pair;
+import amie.data.KB;
+import amie.rules.Rule;
+
+public class OAMiningAssistant extends MiningAssistant {
+
+    public OAMiningAssistant(KB datasource){
+        super(datasource);
+
+
+
+
+    }
+
+
+}
